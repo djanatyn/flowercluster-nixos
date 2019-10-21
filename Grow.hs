b9bdaf74d568cd98544f501479315e526403b85c
Jonathan Strickland <djanatyn@gmail.com>
2019-10-20 23:13:33 -0400

    run terraform init with phony "init" target

--- Grow.hs
+++ Grow.hs
@@ -12,12 +12,10 @@ main :: IO ()
 main = shakeArgs shakeOptions $ do
   want ["_build/output.json"]
 
-  "terraform/.terraform" %> \out -> do
+  phony "init" $ do
+    putNormal "initializing terraform"
     cmd (Cwd "terraform") "terraform init"
 
   "_build/output.json" %> \out -> do
-    need ["terraform/.terraform"]
-
     Stdout output <- cmd (Cwd "terraform") "terraform output -json"
     writeFileChanged "_build/output.json" output
-  
