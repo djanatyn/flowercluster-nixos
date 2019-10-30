54982b297ffd895684599ce7ae50b43a034dc86e
Jonathan Strickland <djanatyn@gmail.com>
2019-10-29 22:47:49 -0400

    feat(network): add default route table to vpc

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -14,6 +14,19 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
+resource "aws_default_route_table" "default" {
+  default_route_table_id = aws_vpc.main.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "default"
+  }
+}
+
 resource "aws_subnet" "dmz" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
