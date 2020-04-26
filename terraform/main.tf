35af83d755d6a53d256d114295c3f38378f04b54
Jonathan Strickland <djanatyn@gmail.com>
2020-04-26 00:15:51 -0400

    remove outdated terraform config

--- terraform/main.tf
+++
@@ -1,6 +0,0 @@
-module "network" {
-  source = "./modules/network"
-
-  vpc_name   = var.garden_name
-  public_key = var.garden_key
-}
--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "garden_name" {
-  description = "Name of environment."
-  type        = string
-}
-
-variable "garden_key" {
-  description = "Public RSA key for access."
-  type        = string
-}
