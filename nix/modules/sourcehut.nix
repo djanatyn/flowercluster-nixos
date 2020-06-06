82f5ef07134c116dc5fffc6d068b741a6ac296f2
Jonathan Strickland <djanatyn@gmail.com>
2020-06-06 00:58:10 -0400

    generate sourcehut ini config from nix attrset
    lib.generators from nixpkgs is very cool

--- nix/modules/sourcehut.nix
+++ nix/modules/sourcehut.nix
@@ -7,6 +7,45 @@ in {
   };
 
   config = mkIf cfg.enable {
+    environment.etc."sr.ht/config.ini".text = lib.generators.toINI { } {
+      "sr.ht" = {
+        site-name = "flowercluster.io";
+        site-info = "http://src.flowercluster.io";
+        environment = "development";
+        site-blurb = "garden code and it will grow";
+
+        owner-name = "Jonathan Strickland";
+        owner-email = "djanatyn@gmail.com";
+
+        source-url = "https://git.sr.ht/~sircmpwn/shrt";
+        secret-key = lib.fileContents /var/src/secrets/sourcehut/secret-key;
+      };
+
+      webhooks = {
+        private-key =
+          lib.fileContents /var/src/secrets/sourcehut/webhooks/private-key;
+      };
+
+      "meta.sr.ht" = {
+        origin = "http://src.flowercluster.io";
+        debug-host = "0.0.0.0";
+        debug-port = "5000";
+
+        connection-string = "postgresql://sourcehut@localhost/meta.sr.ht";
+
+        migrate-on-upgrade = "yes";
+        welcome-emails = "no";
+      };
+
+      "meta.sr.ht::settings" = {
+        registration = "no";
+        onboarding-redirect = "http://src.flowercluster.io";
+        user-invites = 5;
+      };
+
+      "meta.sr.ht::billing" = { enabled = "no"; };
+    };
+
     environment.systemPackages = with pkgs; [
       sourcehut.gitsrht
       sourcehut.todosrht
